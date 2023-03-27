2627d3753040390d179ab4de5521eaed03e29889
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-03-27 23:43:00 +0300

    Setup test project packages

---
+++ tests/eShopOnTelegram.Domain.IntegrationTests/Startup.cs
@@ -0,0 +1,26 @@
+﻿using eShopOnTelegram.Persistence.Context;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace eShopOnTelegram.Domain.IntegrationTests;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        var dbPath = Path.Join(path, "eShopOnTelegramTest.db");
+
+        services
+            .AddDbContext<EShopOnTelegramDbContext>(options => options.UseSqlite($"Data Source={dbPath}"))
+            .AddSingleton<ILoggerFactory, LoggerFactory>()
+            .AddTransient(typeof(ILogger<>), typeof(Logger<>));
+
+        services.AddTransient<IBasketService, BasketService>();
+        services.AddTransient<ICatalogService, CatalogService>();
+        services.AddTransient<IOrderService, OrderService>();
+    }
+}
