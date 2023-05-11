cadb9b5a2b252372ba81ad59c222233010ec4ce1
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-05-11 15:50:07 +0300

    Add order creation blank test file

---
+++ tests/eShopOnTelegram.Domain.IntegrationTests/Services/Orders/CreateOrderTests.cs
@@ -0,0 +1,16 @@
+﻿using eShopOnTelegram.Domain.Services.Interfaces;
+using eShopOnTelegram.Persistence.Context;
+
+namespace eShopOnTelegram.Domain.IntegrationTests.Services.Orders;
+
+public class CreateOrderTests
+{
+    private readonly IOrderService _orderService;
+    private readonly EShopOnTelegramDbContext _dbContext;
+
+    public CreateOrderTests(IOrderService orderService, EShopOnTelegramDbContext dbContext)
+    {
+        _orderService = orderService;
+        _dbContext = dbContext;
+    }
+}
