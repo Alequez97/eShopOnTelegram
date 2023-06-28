9078c6233df4720a72702a7d86bb4cee1b0fc6b2
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-06-28 20:45:47 +0300

    Fix admin project api endpoint

--- src/eShopOnTelegram.Admin/ClientApp/src/main.tsx
+++ src/eShopOnTelegram.Admin/ClientApp/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_BASE_URL ?? '/api';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
