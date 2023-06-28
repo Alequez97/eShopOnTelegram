cb14be8288b4ca399c0ebe88b7bb7123a4231a16
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-06-29 02:01:44 +0300

    Add telegram bot unit tests project. Added simle start command case test

---
+++ tests/eShopOnTelegram.TelegramBot.UnitTests/Commands/StartCommandTests.cs
@@ -0,0 +1,50 @@
+﻿using eShopOnTelegram.Domain.Dto.Orders;
+using eShopOnTelegram.Domain.Requests.Customers;
+using eShopOnTelegram.Domain.Responses;
+using eShopOnTelegram.Domain.Services.Interfaces;
+using eShopOnTelegram.TelegramBot.Commands;
+using eShopOnTelegram.TelegramBot.UnitTests.Extensions;
+
+using FluentAssertions;
+
+using Moq;
+using Moq.AutoMock;
+
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace eShopOnTelegram.TelegramBot.UnitTests.Commands;
+
+public class StartCommandTests
+{
+    private AutoMocker _autoMocker;
+    private Update _update;
+
+    [SetUp]
+    public void Setup()
+    {
+        _autoMocker = new AutoMocker();
+        _update = TestData.GetMockedTelegramUpdate();
+    }
+
+    [Test]
+    public async Task WhenCreateCustomerSuccess_And_NoUnpaidOrderFound_SendTextMessageShouldBeCalledOnce()
+    {
+        // Arrange
+        _autoMocker.GetMock<ICustomerService>()
+            .Setup(customerService => customerService.CreateIfNotPresentAsync(It.IsAny<CreateCustomerRequest>()))
+            .ReturnsAsync(new ActionResponse() { Status = ResponseStatus.Success });
+
+        _autoMocker.GetMock<IOrderService>()
+            .Setup(orderService => orderService.GetUnpaidOrderByTelegramIdAsync(It.IsAny<long>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new Response<OrderDto>() { Status = ResponseStatus.NotFound });
+
+        var startCommand = _autoMocker.CreateInstance<StartCommand>();
+
+        // Act
+        await startCommand.SendResponseAsync(_update);
+
+        // Assert
+        _autoMocker.GetMock<ITelegramBotClient>().Invocations.ToList().Should().HaveCount(1);
+    }
+}
---
+++ tests/eShopOnTelegram.TelegramBot.UnitTests/TestData/TestData.cs
@@ -0,0 +1,31 @@
+﻿using Telegram.Bot.Types;
+
+namespace eShopOnTelegram.TelegramBot.UnitTests.Extensions;
+
+public static class TestData
+{
+    public static Update GetMockedTelegramUpdate()
+    {
+        var user = new User()
+        {
+            Id = -1,
+            FirstName = "TestFirstName",
+            LastName = "TestLastName",
+            Username = "TestUserName"
+        };
+
+        var message = new Message()
+        {
+            From = user,
+            Chat = new Chat()
+            {
+                Id = -1
+            }
+        };
+
+        return new Update()
+        {
+            Message = message
+        };
+    }
+}
