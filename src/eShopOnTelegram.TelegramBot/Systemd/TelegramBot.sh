df716fa68411af2124f17d5f85e64658db18a570
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-06-28 22:30:28 +0300

    Fix telegram bot systemd creation script

--- src/eShopOnTelegram.TelegramBot/Systemd/TelegramBot.sh
+++ src/eShopOnTelegram.TelegramBot/Systemd/TelegramBot.sh
@@ -9,6 +9,7 @@ chown root /srv/eShopOnTelegram.TelegramBot
 cd ..
 dotnet publish -c Release -o /srv/eShopOnTelegram.TelegramBot
 
+cd Systemd
 sudo cp eShopOnTelegram.TelegramBot.service /etc/systemd/system/eShopOnTelegram.TelegramBot.service
 sudo systemctl daemon-reload
 echo "run sudo systemctl start eShopOnTelegram.TelegramBot to start service"
