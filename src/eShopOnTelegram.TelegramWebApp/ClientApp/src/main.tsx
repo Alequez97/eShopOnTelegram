0448667d993b8f82285d661d5e0ca66106c11d4d
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-06-28 20:07:49 +0300

    Remove unused package json command and restore build command

--- src/eShopOnTelegram.TelegramWebApp/ClientApp/src/main.tsx
+++ src/eShopOnTelegram.TelegramWebApp/ClientApp/src/main.tsx
@@ -4,7 +4,7 @@ import './main.css';
 import App from './App';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_BASE_URL;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_API_BASE_URL ?? '/api';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
