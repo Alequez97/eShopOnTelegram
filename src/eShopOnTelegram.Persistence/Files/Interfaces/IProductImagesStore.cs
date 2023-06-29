f110179d46b115a76b96a2140208343d6a060957
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-06-29 15:29:13 +0300

    Add product images blob container

---
+++ src/eShopOnTelegram.Persistence/Files/Interfaces/IProductImagesStore.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace eShopOnTelegram.Persistence.Files.Interfaces;
+
+public interface IProductImagesStore
+{
+    public Task<string> SaveAsync(IFormFile image, CancellationToken cancellationToken);
+
+    public Task DeleteAsync(string id);
+}
---
+++ src/eShopOnTelegram.Persistence/Files/Stores/AzureBlobStorageProductImagesStore.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Blobs;
+
+using eShopOnTelegram.Persistence.Files.Interfaces;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace eShopOnTelegram.Persistence.Files.Stores;
+
+public class AzureBlobStorageProductImagesStore : IProductImagesStore
+{
+    private readonly BlobContainerClient _blobContainer;
+
+    public AzureBlobStorageProductImagesStore(IConfiguration configuration)
+    {
+        var connectionString = configuration["Azure:StorageAccountConnectionString"];
+        var blobContainerName = configuration["Azure:ProductImagesBlobContainerName"];
+        
+        var blobServiceClient = new BlobServiceClient(connectionString);
+        _blobContainer = blobServiceClient.GetBlobContainerClient(blobContainerName);
+    }
+
+    public async Task<string> SaveAsync(IFormFile image, CancellationToken cancellationToken)
+    {
+        var imageName = $"{Guid.NewGuid()}.png";
+        var blobClient = _blobContainer.GetBlobClient(imageName);
+
+        await using var imageStream = image.OpenReadStream();
+        await blobClient.UploadAsync(imageStream, cancellationToken);
+
+        return imageName;
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _blobContainer.DeleteBlobIfExistsAsync(id);
+    }
+}
