367e47c29d0cc95f4054058a4dce7c71ff0af2ec
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-07-10 14:44:12 +0300

    Add location to terrafrom output and container instance deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -287,6 +287,10 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
+}
+
 output "admin_app_name" {
   value = azurerm_linux_web_app.admin.name
 }
