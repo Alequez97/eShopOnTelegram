5228876faeaa3720a28d8dc49546cbd9b9931a62
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2023-11-14 16:16:24 +0200

    Create secret update model

--- src/eShopOnTelegram.Admin/Endpoints/Secrets/CreateOrUpdateSecret.cs
+++ src/eShopOnTelegram.Admin/Endpoints/Secrets/CreateOrUpdateSecret.cs
@@ -1,5 +1,44 @@
-﻿namespace eShopOnTelegram.Admin.Endpoints.Secrets;
+﻿using eShopOnTelegram.Admin.Constants;
+using eShopOnTelegram.RuntimeConfiguration.Secrets.Interfaces;
+using eShopOnTelegram.RuntimeConfiguration.Secrets.Requests;
 
-public class CreateOrUpdateSecret
+using Microsoft.AspNetCore.Authorization;
+
+namespace eShopOnTelegram.Admin.Endpoints.Secrets;
+
+public class CreateOrUpdateSecret : EndpointBaseAsync
+    .WithRequest<CreateOrUpdateSecretRequest>
+    .WithActionResult
 {
+    private readonly IKeyVaultClient _keyVaultClient;
+    private readonly ILogger<CreateOrUpdateSecret> _logger;
+
+    public CreateOrUpdateSecret(
+        IKeyVaultClient keyVaultClient,
+        ILogger<CreateOrUpdateSecret> logger)
+    {
+        _keyVaultClient = keyVaultClient;
+        _logger = logger;
+    }
+
+
+    [Authorize(Policy = AuthPolicy.RequireSuperadminClaim)]
+    [HttpPost("/api/secretsConfig")]
+    [SwaggerOperation(Tags = new[] { SwaggerGroup.Secrets })]
+    public override async Task<ActionResult> HandleAsync(CreateOrUpdateSecretRequest request, CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            await _keyVaultClient.CreateOrUpdateAsync(request);
+
+            // TODO: Restart shop application
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            return BadRequest();
+        }
+    }
 }
---
+++ src/eShopOnTelegram.RuntimeConfiguration/Secrets/Requests/CreateOrUpdateSecretRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace eShopOnTelegram.RuntimeConfiguration.Secrets.Requests;
+
+public class CreateOrUpdateSecretRequest
+{
+    public required string PublicSecretName { get; set; }
+    public required string Value { get; set; }
+}
