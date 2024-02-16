c50775cbc6b36585773d21bda43831080a7d69c3
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2024-02-16 13:08:06 +0200

    fix: block scoped namespace for constants

--- src/eShopOnTelegram.Admin/Constants/SwaggerGroup.cs
+++ src/eShopOnTelegram.Admin/Constants/SwaggerGroup.cs
@@ -1,13 +1,12 @@
-﻿namespace eShopOnTelegram.Admin.Constants
+﻿namespace eShopOnTelegram.Admin.Constants;
+
+public static class SwaggerGroup
 {
-	public static class SwaggerGroup
-	{
-		public const string Auth = nameof(Auth);
-		public const string Products = nameof(Products);
-		public const string ProductCategories = nameof(ProductCategories);
-		public const string Customers = nameof(Customers);
-		public const string Orders = nameof(Orders);
-		public const string ApplicationContent = nameof(ApplicationContent);
-		public const string Secrets = nameof(Secrets);
-	}
+	public const string Auth = nameof(Auth);
+	public const string Products = nameof(Products);
+	public const string ProductCategories = nameof(ProductCategories);
+	public const string Customers = nameof(Customers);
+	public const string Orders = nameof(Orders);
+	public const string ApplicationContent = nameof(ApplicationContent);
+	public const string Secrets = nameof(Secrets);
 }
